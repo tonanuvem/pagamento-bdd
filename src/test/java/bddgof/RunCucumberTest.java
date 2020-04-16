5220c0d0f7b8309915f7826704f9c708b5934f08
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-16 11:02:42 -0300

    Update RunCucumberTest.java

--- src/test/java/bddgof/RunCucumberTest.java
+++ src/test/java/bddgof/RunCucumberTest.java
@@ -5,7 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+//@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"}) 
 public class RunCucumberTest {
     
 }
